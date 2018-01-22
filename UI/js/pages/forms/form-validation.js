f879cd460d3d4289d3074d84cbfcdc3367238362
Ashutosh <ashutosh.tripathi@metacube.com>
2018-01-22 21:42:38 +0530

    Initial UI template added.

---
+++ UI/js/pages/forms/form-validation.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('#form_validation').validate({
+        rules: {
+            'checkbox': {
+                required: true
+            },
+            'gender': {
+                required: true
+            }
+        },
+        highlight: function (input) {
+            $(input).parents('.form-line').addClass('error');
+        },
+        unhighlight: function (input) {
+            $(input).parents('.form-line').removeClass('error');
+        },
+        errorPlacement: function (error, element) {
+            $(element).parents('.form-group').append(error);
+        }
+    });
+});
