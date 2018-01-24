fe548d17b8506b7d9b9375e5557812eada4afb32
Ashutosh <test@test.com>
2018-01-24 18:37:21 +0530

    Make sample call with post data.

--- UI/js/pages/forms/addEmp.js
+++ UI/js/pages/forms/addEmp.js
@@ -16,6 +16,31 @@ $(function () {
         },
         errorPlacement: function (error, element) {
             $(element).parents('.form-group').append(error);
-        }
+        },
+		 submitHandler: function(form) {
+			// call your function
+			var empData = {
+				fName:$('#fName').val(),
+				mName:$('#mName').val(),
+				lName:$('#lName').val(),
+				email:$('#email').val(),
+				contactNumber:$('#contactNumber').val(),
+				manager:$('#manager').val(),
+				description:$('#description').val(),
+				
+			}
+			alert(JSON.stringify(data));
+			
+			$.ajax({
+				url: 'localhost:52557/AddEmp',
+				type: 'post',
+				dataType: 'json',
+				success: function (data) {
+					alert('Data Saved');
+				},
+				data: empData
+			});
+		}
+		
     });
 });
