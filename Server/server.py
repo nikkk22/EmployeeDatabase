baca49b495d18793ed64c2eda276a434221aacb6
nikkk22 <jainnikhil28@gmail.com>
2018-02-14 22:54:17 +0530

    Modified server code to handle searching of data
    Modified the server code to handle searching of data and send back to client.

--- Server/server.py
+++ Server/server.py
@@ -4,18 +4,65 @@ import pymongo
 from pymongo import MongoClient
 
 HOST, PORT = '', 52557
-#payLoadData = '{"items": [{"id": "Open", "label":"Open"},{"id": "OpenNew", "label": "Open New"},\
-#               {"id": "ZoomIn", "label": "Zoom In"},{"id":"ZoomOut", "label": "Zoom Out"},\
-#               {"id":"OriginalView","label": "Original View"},{"id":"Find", "label": "Find..."},\
-#               {"id": "FindAgain","label": "Find Again"},{"id": "CopyAgain","label": "Copy Again"},\
-#               {"id": "CopySVG","label": "Copy SVG"},{"id": "ViewSVG", "label":"View SVG"},\
-#               {"id": "ViewSource", "label": "ViewSource"},{"id": "SaveAs", "label": "Save As"},\
-#               {"id": "About", "label": "About Adobe CVGViewer..."}]}'
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((HOST, PORT))
 server_socket.listen(1)
 print ('Starting web-server on port ' + str(PORT))
+
+def sendError(client_socket):
+    http_response = """\
+HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+
+""" + "The employee already exists"
+
+    client_socket.sendall(http_response.encode())
+    client_socket.close()
+
+def sendResponse(client_socket):
+    http_response = """\
+HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+
+""" + "The employee is inserted into the database"
+
+    client_socket.sendall(http_response.encode())
+    client_socket.close()
+
+def sendEmployeeRecord(client_socket, record):
+    print ("Sending back to client")
+    http_response = """\
+HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+
+""" + str(record[0])
+
+    client_socket.sendall(http_response.encode())
+    client_socket.close()
+
+def sendNoDataPresent(client_socket):
+    print ("Sending back to client")
+    http_response = """\
+HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+
+""" + "No data present"
+
+    client_socket.sendall(http_response.encode())
+    client_socket.close()
+
+def sendNames(client_socket, names):
+    print ("Sending back to client")
+    http_response = """\
+HTTP/1.1 200 OK
+Access-Control-Allow-Origin: *
+
+""" + str(names)
+
+    client_socket.sendall(http_response.encode())
+    client_socket.close()
+
 while True:
     #Connecting to mongodb
     client = MongoClient()
@@ -25,7 +72,7 @@ while True:
     client_socket, client_address = server_socket.accept()
     request = client_socket.recv(1024)
     strr = request.decode()
-    #print (repr(strr))
+    print (repr(strr))
     if (strr.find("AddEmp") != -1):
         print("Got request to add an employee detail")
         indexOPAdd = strr.find("fName")
@@ -61,9 +108,13 @@ while True:
         #search for the data if exists in mongodb
         if (db.empDB.find({"email" : email}).count() > 0):
             print ("Found an element already. Not inserting")
+            sendError(client_socket)
         else:
             #insert into mongodb
             db.empDB.insert_one(empData)
+            print ("Data inserted")
+            sendResponse(client_socket)
+        
     elif (strr.find("SearchEmployee") != -1):
         print("Got request to search an employee detail")
         indexOPSearch = strr.find("email")
@@ -74,15 +125,23 @@ while True:
         if (db.empDB.find({"email" : email}).count() > 0):
             print ("Data found. Returning to client")
             ret = db.empDB.find({"email" : email})
+            sendEmployeeRecord(client_socket, ret)
+        else:
+            found = 0
+            count = 0
+            matchedNames = {}
+            ret = db.empDB.find({})
+            for r in ret:
+                if (r['email'].find(email) != -1):
+                    print ("found the data")
+                    found = 1
+                    matchedNames[count] = r['email']
+                    count = count + 1
+            if (found == 0):
+                sendNoDataPresent(client_socket)
+            else:
+                sendNames(client_socket, matchedNames)
     elif (strr != ''):
         print("The request is not valid")
-    
-    http_response = """\
-HTTP/1.1 200 OK
-Access-Control-Allow-Origin: *
 
-""" + str(empData)
-
-    client_socket.sendall(http_response.encode())
-    client_socket.close()
 
