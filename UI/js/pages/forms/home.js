4af54924d59603ce1e0f98dcab1fa1deb43e5868
Ashutosh <test@test.com>
2018-02-21 13:17:15 +0530

    empty popup added for showing employee detail (need to fill later) Code to render list of employee dynamically

--- UI/js/pages/forms/home.js
+++ UI/js/pages/forms/home.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('#forgot_password').validate({
+    $('#employee_search').validate({
         rules: {
             email: {
                 required: true,
@@ -16,22 +16,59 @@ $(function () {
         errorPlacement: function (error, element) {
             $(element).parents('.input-group').append(error);
         },
-        submitHandler: function (form) {
-            console.log('fetching data');
-            // call your function			
-            return;
-
+        submitHandler: function (form, event) {
+		    event.preventDefault();
+			var queryData = $('#email').val();
+			console.log('Searching for email: ' + queryData);	
             $.ajax({
-                url: 'http://localhost:52557/AddEmp',
+                url: 'http://localhost:52557/SearchEmployee?email=' + queryData,
                 type: 'get',
-                //dataType: 'json',
                 success: function (data) {
-                    alert('Success' + data);
+					console.log('Success' + data);
+					if(data.length==0)
+						alert('No match found');
+					else if(data.length==1)		
+					{
+						//Fill Record details 
+						
+						//show popup with emp details
+						$('#empDetail').modal('show'); 	
+					}
+					else
+					{
+						//Fill Table
+						var names = Object.keys(data[0]);
+
+						//Table Header
+						var tbl_header = document.createElement("thead");
+						var tbl_hed_row = tbl_header.insertRow();
+						$.each(names, function (k, v) {
+							var cell = tbl_hed_row.insertCell();
+							cell.appendChild(document.createTextNode(v.toString()));
+						})
+
+						//Table Body
+						var tbl_body = document.createElement("tbody");
+						$.each(data, function () {
+							var tbl_row = tbl_body.insertRow();
+							$.each(this, function (k, v) {
+								var cell = tbl_row.insertCell();
+								var cellVal = (v == null) ? "" : v;
+								cell.appendChild(document.createTextNode(cellVal.toString()));
+							})
+						})
+						
+						//Clear Table
+						$("#empTable").empty();
+						//Add Table Header
+						$("#empTable").append(tbl_header);
+						//Add Table Body
+						$("#empTable").append(tbl_body);
+					}
                 },
                 error: function (data) {
                     alert('Error:' + data);
-                },
-                data: empData
+                }
             });
         }
     });
