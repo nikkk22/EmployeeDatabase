fc6c2e2aa489523a4dfc4f95870eeda15b79214d
Ashutosh <test@test.com>
2018-03-06 12:52:04 +0530

    Display employee detail on row click event.

--- UI/js/pages/forms/home.js
+++ UI/js/pages/forms/home.js
@@ -18,62 +18,75 @@ $(function () {
         },
         submitHandler: function (form, event) {
 		    event.preventDefault();
-			var queryData = $('#email').val();
-			console.log('Searching for email: ' + queryData);	
-            $.ajax({
-                url: 'http://localhost:52557/SearchEmployee?email=' + queryData,
-                type: 'get',
-                success: function (data) {
-					console.log('Success' + data);
-					//Clear Table
-					$("#empTable").empty();
-					data = data.replace(/'/g,'"');
-					data = JSON.parse(data);
-					if(data.length==0)
-						alert('No match found');
-					//else if(data.length==1)		
-					//{
-						//Fill Record details 
-						
-						//show popup with emp details
-					//	$('#empDetail').modal('show'); 	
-					//}
-					else
-					{
-						//Fill Table
-						var names = Object.keys(data[0]);
-
-						//Table Header
-						var tbl_header = document.createElement("thead");
-						var tbl_hed_row = tbl_header.insertRow();
-						$.each(names, function (k, v) {
-							var cell = tbl_hed_row.insertCell();
-							cell.appendChild(document.createTextNode(v.toString()));
-						})
-
-						//Table Body
-						var tbl_body = document.createElement("tbody");
-						$.each(data, function () {
-							var tbl_row = tbl_body.insertRow();
-							$.each(this, function (k, v) {
-								var cell = tbl_row.insertCell();
-								var cellVal = (v == null) ? "" : v;
-								cell.appendChild(document.createTextNode(cellVal.toString()));
-							})
-						})
-						
-						//Clear Table
-						$("#empTable").empty();
-						//Add Table Header
-						$("#empTable").append(tbl_header);
-						//Add Table Body
-						$("#empTable").append(tbl_body);
-					}
-                },
-                error: function (data) {
-                    alert('Error:' + data);
-                }
-            });
+			var emailId = $('#email').val();
+			console.log('Searching for email: ' + emailId);	
+			searchEmployee(emailId);
         }
     });
+	
+	function searchEmployee(emailId)
+	{
+		$.ajax({
+			url: 'http://localhost:52557/SearchEmployee?email=' + emailId,
+			type: 'get',
+			success: function (data) {
+				console.log('Success' + data);
+				//Clear Table
+				$("#empTable").empty();
+				data = data.replace(/'/g,'"');
+				data = JSON.parse(data);
+				if(data.length==0)
+					alert('No match found');
+				//else if(data.length==1)		
+				//{
+					//Fill Record details 
+					
+					//show popup with emp details
+				//	$('#empDetail').modal('show'); 	
+				//}
+				else
+				{
+					//Fill Table
+					var names = Object.keys(data[0]);
+
+					//Table Header
+					var tbl_header = document.createElement("thead");
+					var tbl_hed_row = tbl_header.insertRow();
+					$.each(names, function (k, v) {
+						var cell = tbl_hed_row.insertCell();
+						cell.appendChild(document.createTextNode(v.toString()));
+					})
+
+					//Table Body
+					var tbl_body = document.createElement("tbody");
+					$.each(data, function () {
+						var tbl_row = tbl_body.insertRow();
+						var emailId = this.Email;
+						$(tbl_row).css( 'cursor', 'pointer' );
+						$(tbl_row).attr('title', 'click to see detail');
+						$(tbl_row).click({emailId: this.Email}, reSearchEmployee);						
+						function reSearchEmployee(event){
+							//alert(event.data.emailId);
+							searchEmployee(event.data.emailId);
+						}
+						$.each(this, function (k, v) {
+							var cell = tbl_row.insertCell();
+							var cellVal = (v == null) ? "" : v;
+							cell.appendChild(document.createTextNode(cellVal.toString()));
+						})
+					})
+					
+					//Clear Table
+					$("#empTable").empty();
+					//Add Table Header
+					$("#empTable").append(tbl_header);
+					//Add Table Body
+					$("#empTable").append(tbl_body);
+				}
+			},
+			error: function (data) {
+				alert('Error:' + data);
+			}
+		});	
+	}
 });
